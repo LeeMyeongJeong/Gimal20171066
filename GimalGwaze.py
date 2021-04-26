24c9f2d8aaf0821a26b031aa064df312e005ab6e
LeeMyeongJeong <cjsfyd63332@gmail.com>
2021-04-27 06:46:11 +0900

    Gimal

--- GimalGwaze.py
+++ GimalGwaze.py
@@ -1,51 +1,35 @@
 import random
-num = int(input("로또 숫자 받고싶은만큼 입력하세요 : "))
+num = int(input("로또 몇장드릴까요? : "))
 
-
-print("미래를 바꿔줄 숫자가 나올것입니다.")
 print("-----------절취선-----------")
 for x in range(1, num+1):
-    lotto = [0, 0, 0, 0, 0, 0]
+    l = [0, 0, 0, 0, 0, 0]
 
-    lotto[0] = random.randrange(1, 46, 1)
+    l[0] = random.randrange(1, 46, 1)
 
-    lotto[1] = lotto[0]
-    lotto[2] = lotto[0]
-    lotto[3] = lotto[0]
-    lotto[4] = lotto[0]
-    lotto[5] = lotto[0]
+    l[1] = l[0]
+    l[2] = l[0]
+    l[3] = l[0]
+    l[4] = l[0]
+    l[5] = l[0]
 
-    while (lotto[0] == lotto[1]):
-        lotto[1] = random.randrange(1, 46, 1)
-    while (lotto[0] == lotto[2] or lotto[1] == lotto[2]):
-        lotto[2] = random.randrange(1, 46, 1)
-    while (lotto[0] == lotto[3] or lotto[1] == lotto[3] or lotto[2] == lotto[3]):
-        lotto[3] = random.randrange(1, 46, 1)
-    while (lotto[0] == lotto[4] or lotto[1] == lotto[4] or lotto[2] == lotto[4] or lotto[3] == lotto[4]):
-        lotto[4] = random.randrange(1, 46, 1)
-    while (lotto[0] == lotto[5] or lotto[1] == lotto[5] or lotto[2] == lotto[5] or lotto[3] == lotto[5] or lotto[4] == lotto[5]):
-        lotto[5] = random.randrange(1, 46, 1)
+    while (l[0] == l[1]):
+        l[1] = random.randrange(1, 46, 1)
+    while (l[0] == l[2] or l[1] == l[2]):
+        l[2] = random.randrange(1, 46, 1)
+    while (l[0] == l[3] or l[1] == l[3] or l[2] == l[3]):
+        l[3] = random.randrange(1, 46, 1)
+    while (l[0] == l[4] or l[1] == l[4] or l[2] == l[4] or l[3] == l[4]):
+        l[4] = random.randrange(1, 46, 1)
+    while (l[0] == l[5] or l[1] == l[5] or l[2] == l[5] or l[3] == l[5] or l[4] == l[5]):
+        l[5] = random.randrange(1, 46, 1)
 
 
-    lotto.sort() 
-    print(lotto)
+    l.sort() 
+    print(l)
 print("----------절취선----------")
-print('조상님 저에게 힘을주세요!')
-
-print("딕쇼나리")
-dic = {'test1':'사과',
-       'test2':'배',
-       'test3':'사자'}
-keys = list(dic.keys())
-random.shuffle(keys)
-
-count = 0
-for test in keys:
-    keword = dic[test]
-    g = input('{} 몇번을 테스트할건치 치세요:')
-    if g == keword:
-        print('입니다')
-    else:
-        print('이상한거 하지마세요')
-print("끝났습니다.")        
-#딕쇼나리 어렵덩...
+d = {'d1':'히히 딕셔너리 성공햇덩 ',
+    'd2':'너무조왕 ',
+    'd3':'아이조왕'}    
+print(f'{d["d1"] + d["d2"] + d["d3"]}')    
+print("----------절취선----------")
---
+++ dic.py
@@ -0,0 +1,17 @@
+print("딕쇼나리 테스트")
+dic = {'1':'사자',
+       '2':'배',
+       '3':'사자'}
+
+print(f'동물의왕 {dic["1"]}')
+dic['d1'] = "호랑이"
+print(f'동물의왕 {dic["d1"]}로 교체')
+print(f'물에 뜨는건 {dic["2"]}')
+print(f'엄마 저거 {dic["3"]}')
+print()
+dic['wow'] = "World Of Warcraft"
+print(f'{dic["wow"]}')
+dic['wow'] = "Dirty Hord"
+print(f'{dic["wow"]}')
+dic['wow'] = "Clean Alliance"
+print(f'{dic["wow"]}')
